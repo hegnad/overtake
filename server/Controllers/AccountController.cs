8ebe42b9b6eae40f0e20952ddec552a9b01235b2
Jonathan <152833616+jon-sait@users.noreply.github.com>
2024-08-05 06:55:25 +0800

    Server register input validation

--- server/Controllers/AccountController.cs
+++ server/Controllers/AccountController.cs
@@ -25,7 +25,7 @@ public class AccountController : ControllerBase
     /// </summary>
     [HttpPost]
     [Route("register")]
-    public async Task Register([FromBody] RegisterRequest request)
+    public async Task<IActionResult> RegisterAsync([FromBody] RegisterRequest request)
     {
         _logger.LogTrace(
             "Registration request: username={0}, firstname={1}, lastname={2}, email={3}",
@@ -35,9 +35,19 @@ public class AccountController : ControllerBase
             request.Email
         );
 
-        // TODO: input validation
+        if (string.IsNullOrWhiteSpace(request.Username)
+            || string.IsNullOrWhiteSpace(request.FirstName)
+            || string.IsNullOrWhiteSpace(request.LastName)
+            || string.IsNullOrWhiteSpace(request.Email)
+            || string.IsNullOrWhiteSpace(request.Password))
+        {
+            return new BadRequestResult();
+        }
+
         // TODO: check email/username existence
 
         await _database.InsertAccountAsync(request.Username, request.FirstName, request.LastName, request.Email, request.Password);
+
+        return new OkResult();
     }
 }
