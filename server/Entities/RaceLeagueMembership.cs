c2fb7793d9f13e8f77da9229dfadeb0a71fd0052
dgonc99 <dominic.goncalves@edu.sait.ca>
2024-09-18 16:56:36 -0600

    **METHODS ARE NOT YET TESTED/IMPLEMENTED, DO NOT ATTEMPT PR TO MASTER BRANCH**
    Created read/write interfaces and methods to interact with the following database tables:
    
    - raceLeague
    - raceLeagueMembership
    - leagueInvite

--- server/Entities/RaceLeagueMembership.cs
+++ server/Entities/RaceLeagueMembership.cs
@@ -3,6 +3,6 @@
 public class RaceLeagueMembership
 {
     public required int LeagueId { get; set; }
-    public required int OwnerId { get; set; }
+    public required int UserId { get; set; }
     public required DateTime JoinTime { get; set; }
 }
--- server/Interfaces/IDatabase.cs
+++ server/Interfaces/IDatabase.cs
@@ -9,4 +9,18 @@ public interface IDatabase
     Task<Account> GetAccountByIdAsync(int accountId);
 
     Task<Account> GetAccountByUsernameAsync(string username);
+
+    Task<int> InsertLeagueAsync(int ownerId, string name, bool isPublic, DateTime createTime);
+
+    Task<RaceLeague> GetLeagueByIdAsync(int leagueId);
+
+    Task<RaceLeague> GetLeagueByNameAsync(string name);
+
+    Task<int> InsertLeagueMembershipAsync(int leagueId, int accountId, DateTime joinTime);
+
+    Task<RaceLeagueMembership> GetMembershipByIdAsync(int leagueId);
+
+    Task<int> InsertLeagueInviteAsync(int leagueId, int inviteeId, DateTime requestTime, int status);
+
+    Task<LeagueInvite> GetLeagueInviteByUserIdAsync(int inviteId);
 }
