e9cdf23498603dd94e2020a641d5cacc51eaf591
sebgualtero <147106255+sebgualtero@users.noreply.github.com>
2024-09-20 20:46:37 -0600

    Added new call to bring the GP name

--- web/src/app/utils/api/ergast.js
+++ web/src/app/utils/api/ergast.js
@@ -12,13 +12,20 @@ export async function getNextRace() {
     const raceDate = raceInfo.date;
     const raceTime = raceInfo.time;
     const raceTimeDate = new Date(`${raceDate}T${raceTime}`);
-    const circuitLocation = `${raceInfo.Circuit.Location.locality}, ${raceInfo.Circuit.Location.country}`;
+    const circuitLocation = raceInfo.Circuit.Location.country;
+
+    const apiUrlGPName = `https://api.openf1.org/v1/meetings?year=2024&country_name=${raceInfo.Circuit.Location.country}`;
+
+    const responseGPName = await fetch(apiUrlGPName);
+    const dataGPName = await responseGPName.json();
+    const gpName = dataGPName[0].meeting_official_name;
 
     return {
       raceName,
       circuitName,
       circuitLocation,
       raceTimeDate,
+      gpName,
     };
   } catch (error) {
     console.error("Error fetching race data: ", error);
