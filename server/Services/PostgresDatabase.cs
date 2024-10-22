358caf139a39c7a38488a995575d1b457c86ed3f
sebgualtero <147106255+sebgualtero@users.noreply.github.com>
2024-10-22 14:15:17 -0600

    Update PostgresDatabase.cs
    Missing }

--- server/Services/PostgresDatabase.cs
+++ server/Services/PostgresDatabase.cs
@@ -441,6 +441,8 @@ public class PostgresDatabase : IDatabase
 
         return publicLeagues.ToArray();
 
+    }
+
     // Retrieve driver metadata by driver number
     public async Task<Driver> GetDriverMetadataByNumberAsync(int driverNumber)
     {
