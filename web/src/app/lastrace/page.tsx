74b783319e6ee3c76892142b03603fcdad2ee9e4
sebgualtero <147106255+sebgualtero@users.noreply.github.com>
2024-10-23 18:37:57 -0600

    Completed last race results component and added styling

--- web/src/app/lastrace/page.tsx
+++ web/src/app/lastrace/page.tsx
@@ -10,21 +10,15 @@ import {
   useState,
 } from "react";
 import SidebarLayout from "../ui/sidebar-layout";
-import styles from "../home.module.css";
-import RaceCountdown from "../components/racecountdown";
-import { getNextRace } from "../utils/api/ergast";
+import styles from "./lastrace.module.css";
+import Image from "next/image";
+import { getDriverHeadshot } from "../utils/api/overtake";
 
 export default function LastRace() {
   const [raceResults, setRaceResults] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [nextRace, setNextRace] = useState<null | {
-    raceName: any;
-    circuitName: any;
-    circuitLocation: string;
-    raceTimeDate: Date;
-  }>(null);
-  const [imageUrl, setImageUrl] = useState("");
+  const [winnerImage, setWinnerImage] = useState(null);
 
   // Fetch race results when the component mounts
   useEffect(() => {
@@ -38,38 +32,16 @@ export default function LastRace() {
         }
         const data = await response.json();
         setRaceResults(data.MRData.RaceTable.Races[0]); // Set the last race results
+        const winnerDriverId = data.MRData.RaceTable.Races[0].Results[0].number;
+        const winnerImageUrl = await getDriverHeadshot(winnerDriverId);
+        setWinnerImage(winnerImageUrl);
         setLoading(false);
       } catch (err: any) {
         setError(err.message);
         setLoading(false);
       }
     };
-
-    let driverId = 14;
-
-    const fetchDriverHeadshots = async () => {
-      const response2 = await fetch(
-        "http://localhost:8080/api/driver/headshot/" + driverId,
-        {
-          method: "GET",
-        }
-      );
-      const data2 = await response2.json();
-      console.log(data2);
-      setImageUrl(data2);
-    };
-
     fetchRaceResults();
-    fetchDriverHeadshots();
-  }, []);
-
-  useEffect(() => {
-    async function fetchData() {
-      const race = await getNextRace();
-      setNextRace(race);
-    }
-
-    fetchData();
   }, []);
 
   if (loading)
@@ -91,103 +63,70 @@ export default function LastRace() {
 
   return (
     <SidebarLayout>
-      <div className={styles.driversResults}>
-        <h1 className={styles.title}>Last Race Results</h1>
+      <div className={styles.container}>
         <div>
-          <h2>{raceName}</h2>
-          {/* Method available on Date objects that converts the date to a string using the local settings of the user's browser */}
-          <p>Date: {new Date(date).toLocaleDateString()}</p>
-          <p>Circuit: {circuitName}</p>
-          <p>
-            Location: {Location.locality}, {Location.country}
-          </p>
+          <h1>Last Race Results</h1>
 
-          <h3>Race Results:</h3>
-          <table className="table-fixed border-separate border-spacing-1 border border-slate-500 border-spacing-2 p-5">
-            <thead>
-              <tr>
-                <th className="border border-slate-700 w-16 p-1">Position</th>
-                <th className="border border-slate-700 w-40 p-1">Driver</th>
-                <th className="border border-slate-700 w-40 p-1">
-                  Constructor
-                </th>
-                <th className="border border-slate-700 w-24 p-1">Time</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Results.map(
-                (result: {
-                  Driver: {
-                    driverId: Key | null | undefined;
-                    givenName: string;
-
-                    familyName: string;
-                  };
-                  position: number | undefined;
-                  Constructor: {
-                    name: string | undefined;
-                  };
-                  Time: { time: any };
-                }) => (
-                  <tr key={result.Driver.driverId}>
-                    <td className="border border-slate-700 px-2">
-                      {result.position}
-                    </td>
-                    <td className="border border-slate-700 px-2">
-                      {result.Driver.givenName} {result.Driver.familyName}
-                    </td>
-                    <td className="border border-slate-700 px-2">
-                      {result.Constructor.name}
-                    </td>
-                    <td className="border border-slate-700 px-2">
-                      {result.Time?.time || "+1 Lap"}
-                    </td>
-                  </tr>
-                )
-              )}
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <br></br>
-      <div className={styles.driversResults}>
-        <h1>Next F1 Race</h1>
-        {nextRace ? (
-          <div>
-            <p>
-              <strong>Race: </strong>
-              {(nextRace as any)?.raceName}
-            </p>
-            <p>
-              <strong>Circuit: </strong>
-              {(nextRace as any)?.circuitName}
-            </p>
-            <p>
-              <strong>Location: </strong>
-              {(nextRace as any)?.circuitLocation}
-            </p>
-            <p>
-              <strong>Time & Date: </strong>
-              {nextRace.raceTimeDate.toString()}
-            </p>
+          <div className={styles.header}>
+            <div>
+              <h2>{raceName}</h2>
+              {/* Method available on Date objects that converts the date to a string using the local settings of the user's browser */}
+              <p>Date: {new Date(date).toLocaleDateString()}</p>
+              <p>Circuit: {circuitName}</p>
+              <p>
+                Location: {Location.locality}, {Location.country}
+              </p>
+            </div>
+            <div className={styles.winnerbox}>
+              <h1>Race Winner</h1>
+              <Image
+                src={winnerImage || "Loading..."}
+                alt="Winner"
+                width={200}
+                height={200}
+              />
+            </div>
           </div>
-        ) : (
-          <p>Loading race data...</p>
-        )}
-      </div>
-      <br></br>
-      <div className={styles.driversResults}>
-        <h1>Time to next race:</h1>
-        <RaceCountdown />
-        <div>
-          <img
-            src={imageUrl}
-            style={{
-              display: "block",
-            }}
-            alt="Lewis Hamilton"
-            className={styles.driverImage}
-          />
+
+          <h1>Race Results:</h1>
+          <div className={styles.raceresults}>
+            <table>
+              <thead>
+                <tr>
+                  <th>Position</th>
+                  <th>Driver</th>
+                  <th>Constructor</th>
+                  <th>Time</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Results.map(
+                  (result: {
+                    Driver: {
+                      driverId: Key | null | undefined;
+                      givenName: string;
+
+                      familyName: string;
+                    };
+                    position: number | undefined;
+                    Constructor: {
+                      name: string | undefined;
+                    };
+                    Time: { time: any };
+                  }) => (
+                    <tr key={result.Driver.driverId}>
+                      <td>{result.position}</td>
+                      <td>
+                        {result.Driver.givenName} {result.Driver.familyName}
+                      </td>
+                      <td>{result.Constructor.name}</td>
+                      <td>{result.Time?.time || "+1 Lap"}</td>
+                    </tr>
+                  )
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </SidebarLayout>
